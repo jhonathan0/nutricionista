2e1ed354ec59511880b57c3fdbc531573ff5c866
jhonathan0 <147885506+jhonathan0@users.noreply.github.com>
2024-09-25 09:28:35 -0300

    imc

--- js/principal.js
+++ js/principal.js
@@ -1,1 +1,12 @@
 var titulo = document.querySelector(".titulo").textContent = "Aparecida Nutricionista";
+
+var pacientes = document.querySelectorAll(".paciente");
+for (var i = 0; i < pacientes.length; i++) {
+
+    var paciente = pacientes[i];
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+    var tdImc = paciente.querySelector(".info-imc");
+}
